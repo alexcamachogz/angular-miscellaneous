18302b130d7912558556de70fdebae07ebfecb72
Alejandra Camacho <camacho.ale94@gmail.com>
2020-11-11 14:22:45 -0600

    Project complete

--- src/app/components/user/new-user.component.ts
+++ src/app/components/user/new-user.component.ts
@@ -1,20 +1,15 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core'
+import { ActivatedRoute } from '@angular/router'
 
 @Component({
   selector: 'app-new-user',
-  template: `
-    <p>
-      new-user works!
-    </p>
-  `,
-  styles: [
-  ]
+  template: ` <p>new-user works!</p> `
 })
-export class NewUserComponent implements OnInit {
-
-  constructor() { }
-
-  ngOnInit(): void {
+export class NewUserComponent {
+  constructor(private route: ActivatedRoute) {
+    this.route.parent.params.subscribe((params) => {
+      console.log('Ruta hija - new user')
+      console.log(params)
+    })
   }
-
 }
--- src/app/components/user/user.component.ts
+++ src/app/components/user/user.component.ts
@@ -1,7 +1,15 @@
 import { Component } from '@angular/core'
+import { ActivatedRoute } from '@angular/router'
 
 @Component({
   selector: 'app-user',
   templateUrl: './user.component.html'
 })
-export class UserComponent {}
+export class UserComponent {
+  constructor(private route: ActivatedRoute) {
+    this.route.params.subscribe((params) => {
+      console.log('Ruta padre')
+      console.log(params)
+    })
+  }
+}
